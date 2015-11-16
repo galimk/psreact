9fb786c2f5dfdeec0b88ebc3974226a89dc116e7
Galim Kaudinov <galimk@gmail.com>
2015-11-15 19:20:00 -0700

    fluxing the sample app.

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,21 +1,16 @@
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 var Link = require('react-router').Link;
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
 
 var AuthorPage = React.createClass({
     getInitialState: function () {
         return {
-            authors: []
+            authors: AuthorStore.getAllAuthors()
         };
-    },
-
-    componentDidMount: function () {
-        if (this.isMounted()) {
-            this.setState({authors: AuthorApi.getAllAuthors()});
-        }
     },
 
     render: function () {
