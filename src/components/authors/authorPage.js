d9e6ce80e5ce1a6988cea055318c676cb6e5eb60
Galim Kaudinov <galimk@gmail.com>
2015-11-15 23:14:30 -0700

    no message

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -13,13 +13,26 @@ var AuthorPage = React.createClass({
         };
     },
 
+
+    _onChange: function () {
+        this.setState({authors: AuthorStore.getAllAuthors()});
+    },
+
+    componentWillMount: function () {
+        AuthorStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        AuthorStore.removeChangeListener(this._onChange);
+    },
+
     render: function () {
 
         return (
             <div>
                 <h1>Authors</h1>
                 <Link to="addAuthor" className="btn btn-default">Add Author</Link>
-                <AuthorList authors={this.state.authors} />
+                <AuthorList authors={this.state.authors}/>
             </div>
         );
     }
